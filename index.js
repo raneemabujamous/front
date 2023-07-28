3f438d58ba2b5bc560895ac2f01ae6c1b4981a85
raneemabujamous <raneemabujamous1998@gmail.com>
2023-07-28 23:11:10 +0300

    finish section one

---
+++ index.js
@@ -0,0 +1,8 @@
+<script>
+  const dropdownButton = document.getElementById('dropdownButton');
+  const menu = document.getElementById('menu');
+
+  dropdownButton.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+</script>
