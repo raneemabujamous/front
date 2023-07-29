a175c6f896f517bca8137b51c69c7d4676137622
raneemabujamous <raneemabujamous1998@gmail.com>
2023-07-29 12:09:06 +0300

    add section 2

--- index.js
+++
@@ -1,8 +0,0 @@
-<script>
-  const dropdownButton = document.getElementById('dropdownButton');
-  const menu = document.getElementById('menu');
-
-  dropdownButton.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
-  });
-</script>
